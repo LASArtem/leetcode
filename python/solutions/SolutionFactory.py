d92df35aec1c52ead071b763133f141494ed58b9
aslebedev <lebedev.arts@gmail.com>
2024-03-19 22:02:34 +0200

    feat: added python solution for lc-000003
    * add Solution0003
    
    Issue-id: lc-000003

--- python/solutions/SolutionFactory.py
+++ python/solutions/SolutionFactory.py
@@ -1,8 +1,7 @@
 from solutions.ISolution import ISolution
 from solutions.SolutionDefault import SolutionDefault
 from solutions.Solution0002 import Solution0002
-
-
+from solutions.Solution0003 import Solution0003
 
 class SolutionFactory:
     def createSolution(self, solutionNumber: int) -> ISolution:
@@ -10,5 +9,7 @@ class SolutionFactory:
 
         if solutionNumber==2 :
             returnSolution = Solution0002()
+        elif solutionNumber == 3 :
+            returnSolution = Solution0003()
 
         return returnSolution
