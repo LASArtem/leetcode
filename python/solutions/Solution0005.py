7166484f64123b88711eaf266fad6ba83f48b62e
aslebedev <lebedev.arts@gmail.com>
2024-03-20 18:40:43 +0200

    feat: added python solution for lc-000005
    * add Solution0005
    
    Issue-id: lc-000005

---
+++ python/solutions/Solution0005.py
@@ -0,0 +1,59 @@
+"""
+0005 Longest Palindromic Substring
+Given a string s, return the longest palindromic substring in s.
+
+Example 1:
+    Input: s = "babad"
+    Output: "bab"
+    Explanation: "aba" is also a valid answer.
+
+Example 2:
+    Input: s = "cbbd"
+    Output: "bb"
+
+Constraints:
+1 <= s.length <= 1000
+s consist of only digits and English letters.
+"""
+
+from solutions.ISolution import ISolution
+
+class Solution0005(ISolution):
+    def isReady(self):
+        return True
+    
+    def describeIssue(self):
+        print("[Solution 0005]: Longest Palindromic Substring")
+
+    def run(self):
+        checkingString = "123bbbbbbbbbbbbaaaaaaaaabbbbbbbbbbbbbbbbbbb321" #123321aba123ccc 123bbbbbbbbbbbbaaaaaaaaabbbbbbbbbbbbbbbbbbb321
+        print(f'Input params:')
+        print(f'    1) string: {checkingString}')
+        palindrome = self.longestPalindrome(checkingString)
+        print(f'The palindrome is: {palindrome}')
+
+    def longestPalindrome(self, s: str) -> str:
+        S_LENGTH = len(s)
+        palindrome = s[0]
+
+        startPalindromeIndex = 0
+        endPalindromeIndex = S_LENGTH - 1
+        while startPalindromeIndex < S_LENGTH - len(palindrome):
+            # find equal letters
+            while s[startPalindromeIndex] != s[endPalindromeIndex]:
+                endPalindromeIndex -= 1
+
+            newPalindromeSize = endPalindromeIndex - startPalindromeIndex + 1
+            if startPalindromeIndex >= endPalindromeIndex or len(palindrome) >= newPalindromeSize:
+                startPalindromeIndex += 1
+                endPalindromeIndex = S_LENGTH - 1
+            else:
+                newPalindrome = s[startPalindromeIndex:endPalindromeIndex+1]
+                if newPalindrome == newPalindrome[::-1] :
+                    palindrome = newPalindrome
+                    startPalindromeIndex += 1
+                    endPalindromeIndex = S_LENGTH - 1
+                else:
+                    endPalindromeIndex -= 1
+
+        return palindrome
--- python/solutions/SolutionFactory.py
+++ python/solutions/SolutionFactory.py
@@ -1,15 +1,17 @@
 from solutions.ISolution import ISolution
-from solutions.SolutionDefault import SolutionDefault
 from solutions.Solution0002 import Solution0002
 from solutions.Solution0003 import Solution0003
+from solutions.Solution0005 import Solution0005
 
 class SolutionFactory:
     def createSolution(self, solutionNumber: int) -> ISolution:
-        returnSolution = SolutionDefault()
+        returnSolution = ISolution()
 
         if solutionNumber==2 :
             returnSolution = Solution0002()
         elif solutionNumber == 3 :
             returnSolution = Solution0003()
+        elif solutionNumber == 5 :
+            returnSolution = Solution0005()
 
         return returnSolution
